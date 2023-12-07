1e33c340a2c2af1439a6f7c19c8c1b517a15db46
Arno Kaimbacher <arno.kaimbacher@hotmail.de>
2023-12-07 16:02:48 +0100

    - updated preservation plan - updated contact-ciew-component.vue

--- src/views/contact-view/contact-view-component.vue
+++ src/views/contact-view/contact-view-component.vue
@@ -6,42 +6,55 @@
                 <p class="lead">Want to keep updated or need further information?</p>
                 <hr class="center-line" />
             </div>
-            <div class="column is-half contact-us-details" style="padding-top: 0; margin-top: 0">
-                <h3>Our Location</h3>
-                <h5>
-                    GeoSphere Austria <br />
-                    Neulinggasse 38 <br />
-                    1030 Wien <br />
-                    <!-- <i class="fas fa-mobile-alt"></i> +43-1-7125674 <br />
+            <div class="columns">
+                <div class="column contact-us-details" style="padding-top: 0; margin-top: 0">
+                    <h3>Our Location</h3>
+                    <h5>
+                        GeoSphere Austria <br />
+                        Neulinggasse 38 <br />
+                        1030 Wien <br />
+                        <!-- <i class="fas fa-mobile-alt"></i> +43-1-7125674 <br />
                     <i class="fas fa-envelope-open-text"></i>
                     <a class="social-media" href="mailto:repository@geologie.ac.at"> repository@geologie.ac.at</a><br /> -->
-                </h5>
-                <ul class="social-links">
-                    <li><i class="fas fa-phone-alt pr-2"></i> +43-1-7125674 <br /></li>
-                    <li>
-                        <a class="social-media" href="mailto:kontakt@geosphere.at"><i class="fas fa-envelope pr-2"></i> kontakt(at)geosphere.at </a>
-                    </li>
-                    <li>
-                        <a class="social-media" href="https://twitter.com/GeoSphere_AT" target="_blank">
-                            <i class="fab fa-twitter"></i> twitter.com/GeoSphere_AT
-                        </a>
-                    </li>
-                    <li>
-                        <a class="social-media" href="https://www.facebook.com/GeoSphere.at" target="_blank">
-                            <i class="fab fa-facebook"></i> www.facebook.com/GeoSphere.at
-                        </a>
-                    </li>
-                    <li>
-                        <a class="social-media" href="https://www.instagram.com/GeoSphere_AT" target="_blank">
-                            <i class="fab fa-instagram"></i> www.instagram.com/GeoSphere_AT
-                        </a>
-                    </li>
-                    <li>
-                        <a class="social-media" href="https://at.linkedin.com/company/geosphereaustria" target="_blank">
-                            <i class="fa-brands fa-linkedin"></i> at.linkedin.com/company/geosphereaustria
-                        </a>
-                    </li>
-                </ul>
+                    </h5>
+
+                    <ul class="social-links">
+                        <li><i class="fas fa-phone-alt pr-2"></i> +43-1-7125674 <br /></li>
+                        <li>
+                            <a class="social-media" href="mailto:kontakt@geosphere.at"><i class="fas fa-envelope pr-2"></i> kontakt(at)geosphere.at </a>
+                        </li>
+                        <li>
+                            <a class="social-media" href="https://twitter.com/GeoSphere_AT" target="_blank">
+                                <i class="fab fa-twitter"></i> twitter.com/GeoSphere_AT
+                            </a>
+                        </li>
+                        <li>
+                            <a class="social-media" href="https://www.facebook.com/GeoSphere.at" target="_blank">
+                                <i class="fab fa-facebook"></i> www.facebook.com/GeoSphere.at
+                            </a>
+                        </li>
+                        <li>
+                            <a class="social-media" href="https://www.instagram.com/GeoSphere_AT" target="_blank">
+                                <i class="fab fa-instagram"></i> www.instagram.com/GeoSphere_AT
+                            </a>
+                        </li>
+                        <li>
+                            <a class="social-media" href="https://at.linkedin.com/company/geosphereaustria" target="_blank">
+                                <i class="fa-brands fa-linkedin"></i> at.linkedin.com/company/geosphereaustria
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="column contact-us-details" style="padding-top: 0; margin-top: 0">
+                    <h3>Contact Person</h3>
+                    <h5>
+                        Viktoria Haider <br />
+                        <a class="social-media" href="mailto:repository@geosphere.at"><i class="fas fa-envelope pr-2"></i> repository(at)geosphere.at </a>
+                        <!-- <i class="fas fa-mobile-alt"></i> +43-1-7125674 <br />
+                    <i class="fas fa-envelope-open-text"></i>
+                    <a class="social-media" href="mailto:repository@geologie.ac.at"> repository@geologie.ac.at</a><br /> -->
+                    </h5>
+                </div>
             </div>
         </section>
     </div>
